c10649ebb595d8e4324adc23e0bcc66aacb01567
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2025-12-10 15:46:42 -0800

    Updates

---
+++ static/js/416.34bd3634.chunk.js
@@ -0,0 +1,20 @@
+"use strict";(self.webpackChunkvitalroute_logistics_website=self.webpackChunkvitalroute_logistics_website||[]).push([[416],{9416:(e,t,i)=>{i.r(t),i.d(t,{default:()=>x});var n=i(1319),r=i(4647),s=i(9281),d=i(3614),c=i(5639),l=i(9);const a=(0,l.Ay)("section")`
+  position: relative;
+  padding: 7.5rem 0 3rem;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 1024px) {
+    padding: 5.5rem 0 3rem;
+  }
+`,o=(0,l.Ay)("p")`
+  padding: 0.75rem 0 0.75rem;
+`,m=(0,l.Ay)("div")`
+  max-width: 570px;
+
+  @media only screen and (max-width: 768px) {
+    max-width: 100%;
+  }
+`;var h=i(579);const x=(0,s.C)()((e=>{let{title:t,content:i,button:s,t:l}=e;return(0,h.jsx)(a,{children:(0,h.jsx)(d.q7,{direction:"up",triggerOnce:!0,children:(0,h.jsx)(n.A,{justify:"center",align:"middle",children:(0,h.jsx)(m,{children:(0,h.jsxs)(r.A,{lg:24,md:24,sm:24,xs:24,children:[(0,h.jsx)("h6",{children:l(t)}),(0,h.jsx)(o,{children:l(i)}),s&&(0,h.jsx)(c.$,{name:"submit",onClick:()=>{return e="contact",void document.getElementById(e).scrollIntoView({behavior:"smooth"});var e},children:l(s)})]})})})})})}))}}]);
+//# sourceMappingURL=416.34bd3634.chunk.js.map
